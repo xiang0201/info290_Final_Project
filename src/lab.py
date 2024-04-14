333d27fd9ae4cd1c2369c0047eb6e58d7167c325
xiang0201 <143204988+xiang0201@users.noreply.github.com>
2024-04-14 16:22:57 -0700

    second commit

---
+++ src/lab.py
@@ -0,0 +1,104 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+    if k==0:
+        return 1
+    else:
+        total=1
+        while k:
+            total=total*n
+            k-=1
+            n-=1
+        return total
+
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    total=0
+    x=1
+    while x <= n:
+        if(x % k == 0):
+            print(x)
+            total+=1
+        x=x+1
+    return total
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    sum=0
+    while y:
+        sum=sum+y%10
+        y=y//10
+    return sum
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+    res=False
+    while n:
+        if n%10==8:
+            n=n//10
+            if(n%10==8):
+                res=True
+        else:
+            n=n//10
+    return res
+
